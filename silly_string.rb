0f403321f2acef969d80a57a2bc04530b0315d81
Sarah Silverman <ssilverman1001@gmail.com>
2018-07-23 20:47:17 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,19 +1,25 @@
+puts "My favorite word is wack"
+puts "wack".capitalize
+puts "IM NOT SHOUTING".downcase
+puts "supercalifragilisticexpialidocious".length
+puts "wow" + "mom"
+puts "i really like programming".capitalize 
 
 # Challenge 1: Fill this string in with your favorite word
 
 your_favorite_word = ""
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is wack"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS WACK#{favorite_word_in_all_caps}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "IM NOT SHOUTING".lowercase
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
@@ -21,7 +27,7 @@ puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious"
+big_word = "supercalifragilisticexpialidocious".length
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
@@ -38,11 +44,11 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 # Challenge 6: Capitalize this sentence properly
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
-
+puts "i really like programming".uppercase
 puts proper_sentence
 
 # Challenge 7: String Switcheroo
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
